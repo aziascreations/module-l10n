29dbca12c6ba7d27f32e21ef14987f277b325518
aziascreations <herwin.bozet@gmail.com>
2016-12-10 16:43:25 +0100

    Change the first part of the locales' name
    Changed them to use the ISO 639-3 language code

---
+++ lang/readme.md
@@ -0,0 +1,22 @@
+#W.I.P
+
+Locale naming "rules"
+
+Use the ISO 639-3 code for the first part of the names
+And the domain name of the country where the language is spoken for the last part.
+
+[ISO 639-3 code]_[Country "domain extension"]
+
+##eg:
+French in Belgium:
+fra_BE
+
+English in Canada
+eng_CA
+
+Look these links for more info:
+
+http://multitree.org/search
+http://www-01.sil.org/iso639-3/
+https://github.com/wooorm/iso-639-3
+https://en.wikipedia.org/wiki/List_of_Internet_top-level_domains
--- src/main/test/com/azias/module/l10n/LocalizerTest.java
+++ src/main/test/com/azias/module/l10n/LocalizerTest.java
@@ -3,6 +3,8 @@ package com.azias.module.l10n;
 import java.io.IOException;
 
 import com.azias.module.l10n.Localizer;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class LocalizerTest {
 	private static Localizer localizer;
@@ -17,6 +19,19 @@ public class LocalizerTest {
 			e.printStackTrace();
 		}
 		
-		localizer.dump();
+		//localizer.dump();
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String a = gson.toJson(localizer);
+		System.out.println(a);
+		
+		/*localizer = new Localizer(false);
+		try {
+			localizer.init();
+			localizer.load("test_TEST");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		localizer.dump();/**/
 	}
 }
